33858e66ea8f4a13b601c0386fea45b9aabda3aa
Asahi Lina <lina@asahilina.net>
2022-09-16 21:18:26 +0900

    drm/asahi: object: Fix Debug impl bounds for GPUPointer
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- drivers/gpu/drm/asahi/object.rs
+++ drivers/gpu/drm/asahi/object.rs
@@ -25,7 +25,7 @@ use crate::alloc::Allocation;
 #[repr(C, packed(4))]
 pub(crate) struct GpuPointer<'a, T: ?Sized>(NonZeroU64, PhantomData<&'a T>);
 
-impl<'a, T> fmt::Debug for GpuPointer<'a, T> {
+impl<'a, T: ?Sized> fmt::Debug for GpuPointer<'a, T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let val = self.0;
         f.write_fmt(format_args!("{:#x} ({})", val, core::any::type_name::<T>()))
