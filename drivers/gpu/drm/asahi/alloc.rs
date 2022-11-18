dba06da1fa7dc10c64a95ec44bd5427cd1709fd9
Asahi Lina <lina@asahilina.net>
2022-11-18 19:02:18 +0900

    drm/asahi: alloc: Implement HeapAllocator
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- drivers/gpu/drm/asahi/alloc.rs
+++ drivers/gpu/drm/asahi/alloc.rs
@@ -17,6 +17,7 @@ use crate::mmu;
 use crate::object::{GpuArray, GpuObject, GpuOnlyArray, GpuStruct, GpuWeakPointer};
 
 use alloc::fmt;
+use core::cmp::Ordering;
 use core::fmt::{Debug, Formatter};
 use core::marker::PhantomData;
 use core::mem;
@@ -25,7 +26,14 @@ use core::ptr::NonNull;
 
 const DEBUG_CLASS: DebugFlags = DebugFlags::Alloc;
 
+#[cfg(not(CONFIG_DRM_ASAHI_DEBUG_ALLOCATOR))]
+pub(crate) type DefaultAllocator = HeapAllocator;
+#[cfg(not(CONFIG_DRM_ASAHI_DEBUG_ALLOCATOR))]
+pub(crate) type DefaultAllocation = HeapAllocation;
+
+#[cfg(CONFIG_DRM_ASAHI_DEBUG_ALLOCATOR)]
 pub(crate) type DefaultAllocator = SimpleAllocator;
+#[cfg(CONFIG_DRM_ASAHI_DEBUG_ALLOCATOR)]
 pub(crate) type DefaultAllocation = SimpleAllocation;
 
 pub(crate) trait RawAllocation {
@@ -299,5 +307,283 @@ impl Allocator for SimpleAllocator {
             vm: self.vm.clone(),
             obj,
         })
+    }
+}
+
+pub(crate) struct HeapAllocationInner {
+    dev: AsahiDevice,
+    ptr: Option<NonNull<u8>>,
+    real_size: usize,
+}
+
+pub(crate) struct HeapAllocation(mm::Node<HeapAllocationInner>);
+
+impl HeapAllocation {}
+
+impl Drop for HeapAllocation {
+    fn drop(&mut self) {
+        mod_dev_dbg!(
+            self.device(),
+            "HeapAllocator: drop object @ {:#x}",
+            self.gpu_ptr()
+        );
+    }
+}
+
+impl RawAllocation for HeapAllocation {
+    fn ptr(&self) -> Option<NonNull<u8>> {
+        self.0.ptr
+    }
+    fn gpu_ptr(&self) -> u64 {
+        self.0.start()
+    }
+    fn size(&self) -> usize {
+        self.0.size() as usize
+    }
+    fn device(&self) -> &AsahiDevice {
+        &self.0.dev
+    }
+}
+
+pub(crate) struct HeapAllocator {
+    dev: AsahiDevice,
+    start: u64,
+    end: u64,
+    top: u64,
+    prot: u32,
+    vm: mmu::Vm,
+    min_align: usize,
+    block_size: usize,
+    cpu_maps: bool,
+    backing_objects: Vec<(crate::gem::ObjectRef, u64)>,
+    guard_nodes: Vec<mm::Node<HeapAllocationInner>>,
+    mm: mm::Allocator<HeapAllocationInner>,
+}
+
+impl HeapAllocator {
+    #[allow(clippy::too_many_arguments)]
+    pub(crate) fn new(
+        dev: &AsahiDevice,
+        vm: &mmu::Vm,
+        start: u64,
+        end: u64,
+        min_align: usize,
+        prot: u32,
+        block_size: usize,
+        cpu_maps: bool,
+    ) -> Result<HeapAllocator> {
+        if !min_align.is_power_of_two() {
+            return Err(EINVAL);
+        }
+
+        let mm = mm::Allocator::new(start as u64, (end - start + 1) as u64)?;
+
+        Ok(HeapAllocator {
+            dev: dev.clone(),
+            vm: vm.clone(),
+            start,
+            end,
+            top: start,
+            prot,
+            min_align,
+            block_size: block_size.max(min_align),
+            cpu_maps,
+            backing_objects: Vec::new(),
+            guard_nodes: Vec::new(),
+            mm,
+        })
+    }
+
+    fn add_block(&mut self, size: usize) -> Result {
+        let size_aligned = (size + mmu::UAT_PGSZ - 1) & !mmu::UAT_PGMSK;
+
+        mod_dev_dbg!(
+            &self.dev,
+            "HeapAllocator::add_block: size={:#x} size_al={:#x}",
+            size,
+            size_aligned,
+        );
+
+        let mut obj = crate::gem::new_kernel_object(&self.dev, size_aligned)?;
+        if self.cpu_maps && debug_enabled(DebugFlags::FillAllocations) {
+            obj.vmap()?.as_mut_slice().fill(0xde);
+        }
+
+        let gpu_ptr = self.top;
+        if let Err(e) = obj.map_at(&self.vm, gpu_ptr, self.prot, self.cpu_maps) {
+            dev_err!(
+                &self.dev,
+                "HeapAllocator::add_block: Failed to map at {:#x} ({:?})",
+                gpu_ptr,
+                e
+            );
+            return Err(e);
+        }
+
+        self.backing_objects.try_reserve(1)?;
+
+        let mut new_top = self.top + size_aligned as u64;
+        if self.cpu_maps {
+            let guard = self.min_align.max(mmu::UAT_PGSZ);
+            mod_dev_dbg!(
+                &self.dev,
+                "HeapAllocator::add_block: Adding guard node {:#x}:{:#x}",
+                new_top,
+                guard
+            );
+
+            let inner = HeapAllocationInner {
+                dev: self.dev.clone(),
+                ptr: None,
+                real_size: guard,
+            };
+
+            let node = match self.mm.reserve_node(inner, new_top, guard as u64, 0) {
+                Ok(a) => a,
+                Err(a) => {
+                    dev_err!(
+                        &self.dev,
+                        "HeapAllocator::add_block: Failed to reserve guard node {:#x}:{:#x}: {:?}",
+                        guard,
+                        new_top,
+                        a
+                    );
+                    return Err(EIO);
+                }
+            };
+
+            self.guard_nodes.try_push(node)?;
+
+            new_top += guard as u64;
+        }
+        mod_dev_dbg!(&self.dev, "HeapAllocator::add_block: top={:#x}", new_top);
+
+        self.backing_objects.try_push((obj, gpu_ptr))?;
+        self.top = new_top;
+
+        Ok(())
+    }
+
+    fn find_obj(&mut self, addr: u64) -> Result<&mut (crate::gem::ObjectRef, u64)> {
+        let idx = self
+            .backing_objects
+            .binary_search_by(|obj| {
+                let start = obj.1;
+                let end = obj.1 + obj.0.size() as u64;
+                if start > addr {
+                    Ordering::Greater
+                } else if end <= addr {
+                    Ordering::Less
+                } else {
+                    Ordering::Equal
+                }
+            })
+            .or(Err(ENOENT))?;
+
+        Ok(&mut self.backing_objects[idx])
+    }
+}
+
+impl Allocator for HeapAllocator {
+    type Raw = HeapAllocation;
+
+    fn device(&self) -> &AsahiDevice {
+        &self.dev
+    }
+
+    fn alloc(&mut self, size: usize, align: usize) -> Result<HeapAllocation> {
+        if align != 0 && !align.is_power_of_two() {
+            return Err(EINVAL);
+        }
+        let align = self.min_align.max(align);
+        let size_aligned = (size + align - 1) & !(align - 1);
+
+        mod_dev_dbg!(
+            &self.dev,
+            "HeapAllocator::new: size={:#x} size_al={:#x}",
+            size,
+            size_aligned,
+        );
+
+        let inner = HeapAllocationInner {
+            dev: self.dev.clone(),
+            ptr: None,
+            real_size: size,
+        };
+
+        let mut node =
+            match self.mm.insert_node_generic(
+                inner,
+                size_aligned as u64,
+                align as u64,
+                0,
+                mm::InsertMode::Best,
+            ) {
+                Ok(a) => a,
+                Err(a) => {
+                    dev_err!(
+                    &self.dev,
+                    "HeapAllocator::new: Failed to insert node of size {:#x} / align {:#x}: {:?}",
+                    size_aligned, align, a
+                );
+                    return Err(a);
+                }
+            };
+
+        let mut new_object = false;
+        let start = node.start();
+        let end = start + node.size();
+        if start >= self.top {
+            if start > self.top {
+                dev_warn!(
+                    self.dev,
+                    "HeapAllocator::alloc: top={:#x}, start={:#x}",
+                    self.top,
+                    start
+                );
+            }
+            let block_size = self.block_size.max((end - self.top) as usize);
+            self.add_block(block_size)?;
+            new_object = true;
+        }
+        assert!(end <= self.top);
+
+        if self.cpu_maps {
+            mod_dev_dbg!(self.dev, "HeapAllocator::alloc: mapping to CPU",);
+
+            let obj = if new_object {
+                self.backing_objects.last_mut().unwrap()
+            } else {
+                match self.find_obj(start) {
+                    Ok(a) => a,
+                    Err(_) => {
+                        dev_warn!(
+                            self.dev,
+                            "HeapAllocator::alloc: Failed to find object at {:#x}",
+                            start
+                        );
+                        return Err(EIO);
+                    }
+                }
+            };
+            assert!(obj.1 <= start);
+            assert!(obj.1 + obj.0.size() as u64 >= end);
+            let p = obj.0.vmap()?.as_mut_ptr() as *mut u8;
+            node.ptr = NonNull::new(unsafe { p.add((start - obj.1) as usize) });
+            mod_dev_dbg!(
+                self.dev,
+                "HeapAllocator::alloc: CPU pointer = {:?}",
+                node.ptr
+            );
+        }
+
+        mod_dev_dbg!(
+            self.dev,
+            "HeapAllocator::alloc: Allocated {:#x} bytes @ {:#x}",
+            end - start,
+            start
+        );
+
+        Ok(HeapAllocation(node))
     }
 }
