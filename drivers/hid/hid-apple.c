490988c11c470e9745d7db5cd00c812dc8e07cbc
Janne Grunau <j@jannau.net>
2022-12-11 18:32:12 +0100

    fixup! HID: apple: add support for internal keyboards
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/hid/hid-apple.c
+++ drivers/hid/hid-apple.c
@@ -1110,7 +1110,7 @@ static const struct hid_device_id apple_devices[] = {
 	{ HID_BLUETOOTH_DEVICE(BT_VENDOR_ID_APPLE, USB_DEVICE_ID_APPLE_MAGIC_KEYBOARD_NUMPAD_2021),
 		.driver_data = APPLE_HAS_FN | APPLE_ISO_TILDE_QUIRK },
 	{ HID_SPI_DEVICE(SPI_VENDOR_ID_APPLE, HID_ANY_ID),
-		.driver_data = APPLE_HAS_FN },
+		.driver_data = APPLE_HAS_FN | APPLE_ISO_TILDE_QUIRK },
 	{ HID_DEVICE(BUS_HOST, HID_GROUP_ANY, HOST_VENDOR_ID_APPLE,
                      HID_ANY_ID), .driver_data = APPLE_HAS_FN },
 
