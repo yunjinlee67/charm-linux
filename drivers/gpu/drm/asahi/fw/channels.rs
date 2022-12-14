lfnpcsoft/linux-asahi

drm/asahi: Add initial firmware types & initdata
Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/gpu/drm/asahi/fw/channels.rs
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+#![allow(missing_docs)]
+
+//! GPU communication channels (ring buffers)
+
+use super::types::*;
+
+pub(crate) mod raw {
+    use super::*;
+
+    #[derive(Debug, Default)]
+    #[repr(C)]
+    pub(crate) struct ChannelState<'a> {
+        pub(crate) read_ptr: AtomicU32,
+        __pad0: Pad<0x1c>,
+        pub(crate) write_ptr: AtomicU32,
+        __pad1: Pad<0xc>,
+        _p: PhantomData<&'a ()>,
+    }
+
+    #[derive(Debug, Default)]
+    #[repr(C)]
+    pub(crate) struct FWCtlChannelState<'a> {
+        pub(crate) read_ptr: AtomicU32,
+        __pad0: Pad<0xc>,
+        pub(crate) write_ptr: AtomicU32,
+        __pad1: Pad<0xc>,
+        _p: PhantomData<&'a ()>,
+    }
+}
+
+#[derive(Debug, Default)]
+pub(crate) struct ChannelState {}
+
+impl GPUStruct for ChannelState {
+    type Raw<'a> = raw::ChannelState<'a>;
+}
+
+#[derive(Debug, Default)]
+pub(crate) struct FWLogChannelState {}
+
+impl FWLogChannelState {
+    const SUB_CHANNELS: usize = 6;
+}
+
+impl GPUStruct for FWLogChannelState {
+    type Raw<'a> = Array<{ Self::SUB_CHANNELS }, raw::ChannelState<'a>>;
+}
+
+#[derive(Debug, Default)]
+pub(crate) struct FWCtlChannelState {}
+
+impl GPUStruct for FWCtlChannelState {
+    type Raw<'a> = raw::FWCtlChannelState<'a>;
+}
+
+pub(crate) type RunCmdQueueMsg = Array<0x30, u8>;
+pub(crate) type DeviceControlMsg = Array<0x30, u8>;
+pub(crate) type EventMsg = Array<0x38, u8>;
+pub(crate) type FWLogMsg = Array<0xd8, u8>;
+pub(crate) type KTraceMsg = Array<0x38, u8>;
+pub(crate) type StatsMsg = Array<0x60, u8>;
+pub(crate) type FWCtlMsg = Array<0x14, u8>;

drivers/gpu/drm/asahi/fw/types.rs
@@ -1,0 +1,125 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+#![allow(missing_docs)]
+#![allow(unused_imports)]
+
+//! Common types for firmware structure definitions
+
+pub(crate) use kernel::macros::versions;
+use crate::{object, alloc};
+
+use core::sync::atomic;
+use core::fmt;
+use core::ops::{Index, IndexMut, Deref, DerefMut};
+pub(crate) use ::alloc::boxed::Box;
+pub(crate) use core::sync::atomic::{AtomicU64, AtomicU32, AtomicU16, AtomicU8};
+pub(crate) use crate::object::{GPUStruct, GPUPointer};
+pub(crate) type GPUObject<T> = object::GPUObject<T, alloc::SimpleAllocation<T>>;
+pub(crate) type GPUArray<T> = object::GPUArray<T, alloc::SimpleAllocation<T>>;
+pub(crate) use crate::alloc::Allocator as _Allocator;
+pub(crate) type Allocator = alloc::SimpleAllocator;
+pub(crate) use core::marker::PhantomData;
+
+#[derive(Default, Debug, Copy, Clone)]
+pub(crate) struct F32(u32);
+
+impl F32 {
+    pub(crate) const fn new(v: f32) -> F32 {
+        F32(v.to_bits())
+    }
+}
+
+#[macro_export]
+macro_rules! const_f32 {
+    ($val:expr) => {
+        {
+            const _K: F32 = F32::new($val);
+            _K
+        }
+    };
+}
+
+#[derive(Copy, Clone)]
+#[repr(C, packed)]
+pub(crate) struct Pad<const N: usize>([u8; N]);
+
+impl<const N: usize> Default for Pad<N> {
+    fn default() -> Self {
+        Pad([0; N])
+    }
+}
+
+impl<const N: usize> fmt::Debug for Pad<N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!("<pad>"))
+    }
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub(crate) struct Array<const N: usize, T: Sized>([T; N]);
+
+impl<const N: usize, T: Sized> Array<N, T> {
+    pub(crate) fn new(data: [T; N]) -> Self {
+        Self(data)
+    }
+}
+
+impl<const N: usize, T: Sized + Default + Copy> Default for Array<N, T> {
+    fn default() -> Self {
+        Array([Default::default(); N])
+    }
+}
+
+impl<const N: usize, T: Sized> Index<usize> for Array<N, T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl<const N: usize, T: Sized> IndexMut<usize> for Array<N, T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+impl<const N: usize, T: Sized> Deref for Array<N, T> {
+    type Target = [T; N];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const N: usize, T: Sized> DerefMut for Array<N, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<const N: usize, T: Sized + fmt::Debug> fmt::Debug for Array<N, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+/*
+#[derive(Debug, Default)]
+#[repr(transparent)]
+struct Atomic<T>(T);
+
+impl<T: Sized> Deref for Array<N, T> {
+    type Target = [T; N];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const N: usize, T: Sized> DerefMut for Array<N, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+*/

yunjinlee67/charm-linux

drivers/gpu/drm/asahi/fw/channels.rs
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+#![allow(missing_docs)]
+
+//! GPU communication channels (ring buffers)
+
+use super::types::*;
+
+pub(crate) mod raw {
+    use super::*;
+
+    #[derive(Debug, Default)]
+    #[repr(C)]
+    pub(crate) struct ChannelState<'a> {
+        pub(crate) read_ptr: AtomicU32,
+        __pad0: Pad<0x1c>,
+        pub(crate) write_ptr: AtomicU32,
+        __pad1: Pad<0xc>,
+        _p: PhantomData<&'a ()>,
+    }
+
+    #[derive(Debug, Default)]
+    #[repr(C)]
+    pub(crate) struct FwCtlChannelState<'a> {
+        pub(crate) read_ptr: AtomicU32,
+        __pad0: Pad<0xc>,
+        pub(crate) write_ptr: AtomicU32,
+        __pad1: Pad<0xc>,
+        _p: PhantomData<&'a ()>,
+    }
+}
+
+#[derive(Debug, Default)]
+pub(crate) struct ChannelState {}
+
+impl GpuStruct for ChannelState {
+    type Raw<'a> = raw::ChannelState<'a>;
+}
+
+#[derive(Debug, Default)]
+pub(crate) struct FwLogChannelState {}
+
+impl FwLogChannelState {
+    const SUB_CHANNELS: usize = 6;
+}
+
+impl GpuStruct for FwLogChannelState {
+    type Raw<'a> = Array<{ Self::SUB_CHANNELS }, raw::ChannelState<'a>>;
+}
+
+#[derive(Debug, Default)]
+pub(crate) struct FwCtlChannelState {}
+
+impl GpuStruct for FwCtlChannelState {
+    type Raw<'a> = raw::FwCtlChannelState<'a>;
+}
+
+pub(crate) type RunCmdQueueMsg = Array<0x30, u8>;
+pub(crate) type DeviceControlMsg = Array<0x30, u8>;
+pub(crate) type EventMsg = Array<0x38, u8>;
+pub(crate) type FwLogMsg = Array<0xd8, u8>;
+pub(crate) type KTraceMsg = Array<0x38, u8>;
+pub(crate) type StatsMsg = Array<0x60, u8>;
+pub(crate) type FwCtlMsg = Array<0x14, u8>;

drivers/gpu/drm/asahi/fw/types.rs
@@ -1,0 +1,123 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+#![allow(missing_docs)]
+#![allow(unused_imports)]
+
+//! Common types for firmware structure definitions
+
+use crate::{alloc, object};
+pub(crate) use kernel::macros::versions;
+
+pub(crate) use crate::object::{GpuPointer, GpuStruct};
+pub(crate) use ::alloc::boxed::Box;
+use core::fmt;
+use core::ops::{Deref, DerefMut, Index, IndexMut};
+use core::sync::atomic;
+pub(crate) use core::sync::atomic::{AtomicU16, AtomicU32, AtomicU64, AtomicU8};
+pub(crate) type GpuObject<T> = object::GpuObject<T, alloc::SimpleAllocation<T>>;
+pub(crate) type GpuArray<T> = object::GpuArray<T, alloc::SimpleAllocation<T>>;
+pub(crate) use crate::alloc::Allocator as _Allocator;
+pub(crate) type Allocator = alloc::SimpleAllocator;
+pub(crate) use core::marker::PhantomData;
+
+#[derive(Default, Debug, Copy, Clone)]
+pub(crate) struct F32(u32);
+
+impl F32 {
+    pub(crate) const fn new(v: f32) -> F32 {
+        F32(v.to_bits())
+    }
+}
+
+#[macro_export]
+macro_rules! const_f32 {
+    ($val:expr) => {{
+        const _K: F32 = F32::new($val);
+        _K
+    }};
+}
+
+#[derive(Copy, Clone)]
+#[repr(C, packed)]
+pub(crate) struct Pad<const N: usize>([u8; N]);
+
+impl<const N: usize> Default for Pad<N> {
+    fn default() -> Self {
+        Pad([0; N])
+    }
+}
+
+impl<const N: usize> fmt::Debug for Pad<N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!("<pad>"))
+    }
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub(crate) struct Array<const N: usize, T: Sized>([T; N]);
+
+impl<const N: usize, T: Sized> Array<N, T> {
+    pub(crate) fn new(data: [T; N]) -> Self {
+        Self(data)
+    }
+}
+
+impl<const N: usize, T: Sized + Default + Copy> Default for Array<N, T> {
+    fn default() -> Self {
+        Array([Default::default(); N])
+    }
+}
+
+impl<const N: usize, T: Sized> Index<usize> for Array<N, T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl<const N: usize, T: Sized> IndexMut<usize> for Array<N, T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+impl<const N: usize, T: Sized> Deref for Array<N, T> {
+    type Target = [T; N];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const N: usize, T: Sized> DerefMut for Array<N, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<const N: usize, T: Sized + fmt::Debug> fmt::Debug for Array<N, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+/*
+#[derive(Debug, Default)]
+#[repr(transparent)]
+struct Atomic<T>(T);
+
+impl<T: Sized> Deref for Array<N, T> {
+    type Target = [T; N];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const N: usize, T: Sized> DerefMut for Array<N, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+*/