lfnpcsoft/linux-asahi

drm/asahi: file,driver: Add demo uAPI ioctls
Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/gpu/drm/asahi/driver.rs
@@ -4,7 +4,8 @@
 //! Driver for the Apple AGX GPUs found in Apple Silicon SoCs.
 
 use kernel::{
-    c_str, device, drm, drm::drv, error::Result, io_mem::IoMem, of, platform, prelude::*, sync::Ref,
+    c_str, device, drm, drm::drv, drm::ioctl, error::Result, io_mem::IoMem, of, platform,
+    prelude::*, sync::Ref,
 };
 
 use crate::{file, gem, gpu, hw, mmu};
@@ -57,6 +58,21 @@ impl drv::Driver for AsahiDriver {
 
     const INFO: drv::DriverInfo = INFO;
     const FEATURES: u32 = drv::FEAT_GEM | drv::FEAT_RENDER;
+
+    kernel::declare_drm_ioctls! {
+        (ASAHI_SUBMIT,          drm_asahi_submit,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::submit),
+        (ASAHI_WAIT_BO,         drm_asahi_wait_bo,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::wait_bo),
+        (ASAHI_CREATE_BO,       drm_asahi_create_bo,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::create_bo),
+        (ASAHI_MMAP_BO,         drm_asahi_mmap_bo,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::mmap_bo),
+        (ASAHI_GET_PARAM,       drm_asahi_get_param,
+                          ioctl::RENDER_ALLOW, file::File::get_param),
+        (ASAHI_GET_BO_OFFSET,   drm_asahi_get_bo_offset,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::get_bo_offset),
+    }
 }
 
 impl platform::Driver for AsahiDriver {

drivers/gpu/drm/asahi/file.rs
@@ -6,12 +6,19 @@
 //! Asahi File state
 
 use crate::driver::AsahiDevice;
-use crate::{driver, gpu};
-use kernel::drm;
+use crate::{alloc, buffer, driver, gem, gpu, mmu, render};
+use kernel::drm::gem::BaseObject;
 use kernel::prelude::*;
-use kernel::sync::Ref;
+use kernel::sync::{smutex::Mutex, Ref};
+use kernel::{bindings, drm};
 
-pub(crate) struct File {}
+pub(crate) struct File {
+    vm: mmu::VM,
+    ualloc: Ref<Mutex<alloc::SimpleAllocator>>,
+    renderer: Box<dyn render::Renderer>,
+}
+
+type DRMFile = drm::file::File<File>;
 
 impl drm::file::DriverFile for File {
     type Driver = driver::AsahiDriver;
@@ -19,6 +26,108 @@ impl drm::file::DriverFile for File {
     fn open(device: &AsahiDevice) -> Result<Box<Self>> {
         dev_info!(device, "DRM device opened");
 
-        Ok(Box::try_new(Self {})?)
+        let vm = device.data().gpu.new_vm()?;
+        let ualloc = Ref::try_new(Mutex::new(alloc::SimpleAllocator::new_with_range(
+            device,
+            &vm,
+            0x60_00000000,
+            0x60_ffffffff,
+            mmu::PROT_GPU_FW_SHARED_RW,
+            buffer::PAGE_SIZE,
+        )))?;
+        let renderer = device.data().gpu.new_renderer(ualloc.clone())?;
+
+        Ok(Box::try_new(Self {
+            vm,
+            ualloc,
+            renderer,
+        })?)
+    }
+}
+
+impl File {
+    pub(crate) fn submit(
+        device: &AsahiDevice,
+        data: &mut bindings::drm_asahi_submit,
+        file: &DRMFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: submit");
+        let inner = file.inner();
+        inner.renderer.render(device, &inner.vm, &inner.ualloc, data)?;
+        Ok(0)
+    }
+
+    pub(crate) fn wait_bo(
+        device: &AsahiDevice,
+        _data: &mut bindings::drm_asahi_wait_bo,
+        _file: &DRMFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: wait_bo");
+        Ok(0)
+    }
+
+    pub(crate) fn create_bo(
+        device: &AsahiDevice,
+        data: &mut bindings::drm_asahi_create_bo,
+        file: &DRMFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: create_bo");
+
+        let mut bo = gem::new_object(device, data.size as usize, data.flags)?;
+
+        if data.flags & bindings::ASAHI_BO_PIPELINE != 0 {
+            let iova = bo.map_into_range(
+                &file.inner().vm,
+                0x11_00000000,
+                0x11_ffffffff,
+                mmu::UAT_PGSZ as u64,
+                mmu::PROT_GPU_SHARED_RW,
+            )?;
+            data.offset = iova as u64 - 0x11_00000000;
+        } else {
+            let iova = bo.map_into_range(
+                &file.inner().vm,
+                0x15_00000000,
+                0x1f_ffffffff,
+                mmu::UAT_PGSZ as u64,
+                mmu::PROT_GPU_SHARED_RW,
+            )?;
+            data.offset = iova as u64;
+        }
+
+        let handle = bo.gem.create_handle(file)?;
+        data.handle = handle;
+
+        Ok(0)
+    }
+
+    pub(crate) fn mmap_bo(
+        device: &AsahiDevice,
+        data: &mut bindings::drm_asahi_mmap_bo,
+        file: &DRMFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: mmap_bo");
+
+        let bo = gem::Object::lookup_handle(file, data.handle)?;
+
+        data.offset = bo.create_mmap_offset()?;
+        Ok(0)
+    }
+
+    pub(crate) fn get_param(
+        device: &AsahiDevice,
+        _data: &mut bindings::drm_asahi_get_param,
+        _file: &DRMFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: get_param");
+        Ok(0)
+    }
+    pub(crate) fn get_bo_offset(
+        device: &AsahiDevice,
+        _data: &mut bindings::drm_asahi_get_bo_offset,
+        _file: &DRMFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: get_bo_offset");
+        Ok(0)
     }
 }

yunjinlee67/charm-linux

drivers/gpu/drm/asahi/driver.rs
@@ -4,7 +4,8 @@
 //! Driver for the Apple AGX GPUs found in Apple Silicon SoCs.
 
 use kernel::{
-    c_str, device, drm, drm::drv, error::Result, io_mem::IoMem, of, platform, prelude::*, sync::Arc,
+    c_str, device, drm, drm::drv, drm::ioctl, error::Result, io_mem::IoMem, of, platform,
+    prelude::*, sync::Arc,
 };
 
 use crate::{file, gem, gpu, hw, mmu};
@@ -57,6 +58,21 @@ impl drv::Driver for AsahiDriver {
 
     const INFO: drv::DriverInfo = INFO;
     const FEATURES: u32 = drv::FEAT_GEM | drv::FEAT_RENDER;
+
+    kernel::declare_drm_ioctls! {
+        (ASAHI_SUBMIT,          drm_asahi_submit,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::submit),
+        (ASAHI_WAIT_BO,         drm_asahi_wait_bo,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::wait_bo),
+        (ASAHI_CREATE_BO,       drm_asahi_create_bo,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::create_bo),
+        (ASAHI_MMAP_BO,         drm_asahi_mmap_bo,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::mmap_bo),
+        (ASAHI_GET_PARAM,       drm_asahi_get_param,
+                          ioctl::RENDER_ALLOW, file::File::get_param),
+        (ASAHI_GET_BO_OFFSET,   drm_asahi_get_bo_offset,
+            ioctl::AUTH | ioctl::RENDER_ALLOW, file::File::get_bo_offset),
+    }
 }
 
 impl platform::Driver for AsahiDriver {

drivers/gpu/drm/asahi/file.rs
@@ -7,12 +7,19 @@
 //! Asahi File state
 
 use crate::driver::AsahiDevice;
-use crate::{driver, gpu};
-use kernel::drm;
+use crate::{alloc, buffer, driver, gem, gpu, mmu, render};
+use kernel::drm::gem::BaseObject;
 use kernel::prelude::*;
-use kernel::sync::Arc;
+use kernel::sync::{smutex::Mutex, Arc};
+use kernel::{bindings, drm};
 
-pub(crate) struct File {}
+pub(crate) struct File {
+    vm: mmu::Vm,
+    ualloc: Arc<Mutex<alloc::SimpleAllocator>>,
+    renderer: Box<dyn render::Renderer>,
+}
+
+type DrmFile = drm::file::File<File>;
 
 impl drm::file::DriverFile for File {
     type Driver = driver::AsahiDriver;
@@ -20,6 +27,110 @@ impl drm::file::DriverFile for File {
     fn open(device: &AsahiDevice) -> Result<Box<Self>> {
         dev_info!(device, "DRM device opened");
 
-        Ok(Box::try_new(Self {})?)
+        let vm = device.data().gpu.new_vm()?;
+        let ualloc = Arc::try_new(Mutex::new(alloc::SimpleAllocator::new_with_range(
+            device,
+            &vm,
+            0x60_00000000,
+            0x60_ffffffff,
+            mmu::PROT_GPU_FW_SHARED_RW,
+            buffer::PAGE_SIZE,
+        )))?;
+        let renderer = device.data().gpu.new_renderer(ualloc.clone())?;
+
+        Ok(Box::try_new(Self {
+            vm,
+            ualloc,
+            renderer,
+        })?)
+    }
+}
+
+impl File {
+    pub(crate) fn submit(
+        device: &AsahiDevice,
+        data: &mut bindings::drm_asahi_submit,
+        file: &DrmFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: submit");
+        let inner = file.inner();
+        inner
+            .renderer
+            .render(device, &inner.vm, &inner.ualloc, data)?;
+        Ok(0)
+    }
+
+    pub(crate) fn wait_bo(
+        device: &AsahiDevice,
+        _data: &mut bindings::drm_asahi_wait_bo,
+        _file: &DrmFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: wait_bo");
+        Ok(0)
+    }
+
+    pub(crate) fn create_bo(
+        device: &AsahiDevice,
+        data: &mut bindings::drm_asahi_create_bo,
+        file: &DrmFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: create_bo");
+
+        let mut bo = gem::new_object(device, data.size as usize, data.flags)?;
+
+        if data.flags & bindings::ASAHI_BO_PIPELINE != 0 {
+            let iova = bo.map_into_range(
+                &file.inner().vm,
+                0x11_00000000,
+                0x11_ffffffff,
+                mmu::UAT_PGSZ as u64,
+                mmu::PROT_GPU_SHARED_RW,
+            )?;
+            data.offset = iova as u64 - 0x11_00000000;
+        } else {
+            let iova = bo.map_into_range(
+                &file.inner().vm,
+                0x15_00000000,
+                0x1f_ffffffff,
+                mmu::UAT_PGSZ as u64,
+                mmu::PROT_GPU_SHARED_RW,
+            )?;
+            data.offset = iova as u64;
+        }
+
+        let handle = bo.gem.create_handle(file)?;
+        data.handle = handle;
+
+        Ok(0)
+    }
+
+    pub(crate) fn mmap_bo(
+        device: &AsahiDevice,
+        data: &mut bindings::drm_asahi_mmap_bo,
+        file: &DrmFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: mmap_bo");
+
+        let bo = gem::Object::lookup_handle(file, data.handle)?;
+
+        data.offset = bo.create_mmap_offset()?;
+        Ok(0)
+    }
+
+    pub(crate) fn get_param(
+        device: &AsahiDevice,
+        _data: &mut bindings::drm_asahi_get_param,
+        _file: &DrmFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: get_param");
+        Ok(0)
+    }
+    pub(crate) fn get_bo_offset(
+        device: &AsahiDevice,
+        _data: &mut bindings::drm_asahi_get_bo_offset,
+        _file: &DrmFile,
+    ) -> Result<u32> {
+        dev_info!(device, "IOCTL: get_bo_offset");
+        Ok(0)
     }
 }