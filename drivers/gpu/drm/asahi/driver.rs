lfnpcsoft/linux-asahi

drm/asahi: Refactor GPU management & initdata, add Stats channel
Sorry, this is too painful to break up into separate commits at this
point...

Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/gpu/drm/asahi/driver.rs
@@ -4,21 +4,11 @@
 //! Driver for the Apple AGX GPUs found in Apple Silicon SoCs.
 
 use kernel::{
-    c_str,
-    device,
-    drm,
-    drm::drv,
-    error::Result,
-    io_mem::IoMem,
-    module_platform_driver, of, platform,
-    prelude::*,
-    soc::apple::rtkit,
-    sync::Ref,
-    sync::smutex::Mutex,
-    PointerWrapper,
+    c_str, device, drm, drm::drv, error::Result, io_mem::IoMem, of,
+    platform, prelude::*, sync::Ref,
 };
 
-use crate::{initdata, alloc, mmu, fw, gem};
+use crate::{gem, gpu, hw, mmu};
 
 use kernel::macros::vtable;
 
@@ -37,9 +27,7 @@ const INFO: drv::DriverInfo = drv::DriverInfo {
 
 pub(crate) struct AsahiData {
     dev: device::Device,
-    uat: crate::mmu::UAT,
-    rtkit: Mutex<Option<rtkit::RTKit<AsahiDevice>>>,
-    initdata: Mutex<fw::types::GPUObject<fw::initdata::InitDataG13GV13_0b4>>,
+    gpu: Ref<dyn gpu::GPUManager>,
 }
 
 pub(crate) struct AsahiResources {
@@ -61,27 +49,6 @@ impl AsahiDevice {
 }
 
 #[vtable]
-impl rtkit::Operations for AsahiDevice {
-    type Data = Ref<DeviceData>;
-    type Buffer = gem::ObjectRef;
-
-    fn shmem_alloc(
-        data: <Self::Data as PointerWrapper>::Borrowed<'_>,
-        size: usize,
-    ) -> Result<Self::Buffer> {
-        let mut guard = data.registrations().ok_or(ENXIO)?;
-        let reg = guard.as_pinned_mut();
-        let dev = reg.device();
-        dev_info!(dev, "shmem_alloc() {:#x} bytes\n", size);
-
-        let mut obj = gem::new_object(dev, size)?;
-        obj.vmap()?;
-        let map = obj.map_into(data.uat.kernel_context())?;
-        dev_info!(dev, "shmem_alloc() -> VA {:#x}\n", map.iova());
-        Ok(obj)
-    }}
-
-#[vtable]
 impl drv::Driver for AsahiDevice {
     type Data = ();
     type Object = gem::Object;
@@ -97,7 +64,10 @@ impl platform::Driver for AsahiDevice {
         (of::DeviceId::Compatible(b"apple,agx-t8103"), None),
     ]}
 
-    fn probe(pdev: &mut platform::Device, _id_info: Option<&Self::IdInfo>) -> Result<Ref<DeviceData>> {
+    fn probe(
+        pdev: &mut platform::Device,
+        _id_info: Option<&Self::IdInfo>,
+    ) -> Result<Ref<DeviceData>> {
         let dev = device::Device::from_dev(pdev);
 
         dev_info!(dev, "Probing!\n");
@@ -110,51 +80,30 @@ impl platform::Driver for AsahiDevice {
 
         let mut res = AsahiResources {
             // SAFETY: This device does DMA via the UAT IOMMU.
-            asc: asc_res
+            asc: asc_res,
         };
 
         // Start the coprocessor CPU, so UAT can initialize the handoff
         AsahiDevice::start_cpu(&mut res)?;
 
-        let uat = mmu::UAT::new(&dev)?;
         let reg = drm::drv::Registration::<AsahiDevice>::new(&dev)?;
-
-        let mut allocator = alloc::SimpleAllocator::new(reg.device(), uat.kernel_context(),
-        0x20);
-        let mut builder = initdata::InitDataBuilderG13GV13_0b4::new(&mut allocator);
-        let initdata = builder.build(&initdata::HWCONFIG_T8103)?;
+        let gpu = gpu::GPUManagerG13GV13_0b4::new(&reg.device(), &hw::t8103::HWCONFIG)?;
 
         let data = kernel::new_device_data!(
             reg,
             res,
             AsahiData {
-                uat,
                 dev,
-                rtkit: Mutex::new(None),
-                initdata: Mutex::new(initdata),
+                gpu,
             },
             "Asahi::Registrations"
         )?;
 
         let data = Ref::<DeviceData>::from(data);
 
-        {
-            let mut guard = data.registrations().ok_or(ENXIO)?;
-            let reg = guard.as_pinned_mut();
-            let dev = reg.device();
-            dev_info!(dev, "info through dev\n");
-        }
+        data.gpu.init()?;
 
-        let mut rtkit = unsafe { rtkit::RTKit::<AsahiDevice>::new(&data.dev, None, 0, data.clone()) }?;
-
-        rtkit.boot()?;
-        *data.rtkit.lock() = Some(rtkit);
-
-        kernel::drm_device_register!(
-            data.registrations().ok_or(ENXIO)?.as_pinned_mut(),
-            (),
-            0
-        )?;
+        kernel::drm_device_register!(data.registrations().ok_or(ENXIO)?.as_pinned_mut(), (), 0)?;
 
         dev_info!(data.dev, "probed!\n");
         Ok(data)

drivers/gpu/drm/asahi/gpu.rs
@@ -1,0 +1,180 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+#![allow(missing_docs)]
+#![allow(dead_code)]
+
+use core::mem;
+
+use kernel::{
+    drm,
+    macros::versions,
+    prelude::*,
+    soc::apple::rtkit,
+    sync::{smutex::Mutex, Ref, UniqueRef},
+    PointerWrapper,
+};
+
+use crate::{alloc, channel, fw, gem, hw, initdata, mmu};
+
+const EP_FIRMWARE: u8 = 0x20;
+const EP_DOORBELL: u8 = 0x21;
+
+const MSG_INIT: u64 = 0x81 << 48;
+const INIT_DATA_MASK: u64 = (1 << 44) - 1;
+
+pub(crate) struct KernelAllocators {
+    pub(crate) private: alloc::SimpleAllocator,
+    pub(crate) shared: alloc::SimpleAllocator,
+    pub(crate) gpu: alloc::SimpleAllocator,
+}
+
+struct RXChannels {
+    stats: channel::StatsChannel,
+}
+
+#[versions(AGX)]
+pub(crate) struct GPUManager {
+    dev: drm::device::Device,
+    initialized: bool,
+    initdata: fw::types::GPUObject<fw::initdata::InitData::ver>,
+    uat: mmu::UAT,
+    alloc: KernelAllocators,
+    io_mappings: Vec<mmu::Mapping>,
+    rtkit: Mutex<Option<rtkit::RTKit<GPUManager::ver>>>,
+    rx_channels: Mutex<RXChannels>,
+}
+
+pub(crate) trait GPUManager: Send + Sync {
+    fn init(&self) -> Result;
+    fn alloc(&mut self) -> &mut KernelAllocators;
+}
+
+#[versions(AGX)]
+#[vtable]
+impl rtkit::Operations for GPUManager::ver {
+    type Data = Ref<GPUManager::ver>;
+    type Buffer = gem::ObjectRef;
+
+    fn recv_message(data: <Self::Data as PointerWrapper>::Borrowed<'_>, ep: u8, msg: u64) {
+        let dev = &data.dev;
+        dev_info!(dev, "RTKit message: {:#x}:{:#x}\n", ep, msg);
+
+        let mut ch = data.rx_channels.lock();
+        ch.stats.poll();
+    }
+
+    fn shmem_alloc(
+        data: <Self::Data as PointerWrapper>::Borrowed<'_>,
+        size: usize,
+    ) -> Result<Self::Buffer> {
+        let dev = &data.dev;
+        dev_info!(dev, "shmem_alloc() {:#x} bytes\n", size);
+
+        let mut obj = gem::new_object(dev, size)?;
+        obj.vmap()?;
+        let map = obj.map_into(data.uat.kernel_context())?;
+        dev_info!(dev, "shmem_alloc() -> VA {:#x}\n", map.iova());
+        Ok(obj)
+    }
+}
+
+#[versions(AGX)]
+impl GPUManager::ver {
+    pub(crate) fn new(
+        dev: &drm::device::Device,
+        cfg: &hw::HWConfig,
+    ) -> Result<Ref<GPUManager::ver>> {
+        let uat = mmu::UAT::new(dev)?;
+        let mut alloc = KernelAllocators {
+            private: alloc::SimpleAllocator::new(dev, uat.kernel_context(), 0x20),
+            shared: alloc::SimpleAllocator::new(dev, uat.kernel_context(), 0x20),
+            gpu: alloc::SimpleAllocator::new(dev, uat.kernel_context(), 0x20),
+        };
+
+        let dyncfg = hw::HWDynConfig {
+            uat_context_table_base: uat.context_table_base(),
+        };
+
+        let mut builder = initdata::InitDataBuilder::ver::new(&mut alloc, &cfg, &dyncfg);
+        let initdata = builder.build()?;
+
+        mem::drop(builder);
+
+        let mut mgr = UniqueRef::try_new(GPUManager::ver {
+            dev: dev.clone(),
+            initialized: false,
+            initdata,
+            uat,
+            io_mappings: Vec::new(),
+            rtkit: Mutex::new(None),
+            rx_channels: Mutex::new(RXChannels {
+                stats: channel::StatsChannel::new(&mut alloc)?,
+            }),
+            alloc,
+        })?;
+
+        let p_stats = mgr.rx_channels.lock().stats.to_raw();
+        mgr.initdata.runtime_pointers.with_mut(|raw, _| {
+            raw.stats = p_stats;
+        });
+
+        for (i, map) in cfg.io_mappings.iter().enumerate() {
+            if let Some(map) = map.as_ref() {
+                mgr.iomap(i, map)?;
+            }
+        }
+
+        let mgr = Ref::from(mgr);
+
+        let rtkit = unsafe { rtkit::RTKit::<GPUManager::ver>::new(dev, None, 0, mgr.clone()) }?;
+
+        *mgr.rtkit.lock() = Some(rtkit);
+
+        return Ok(mgr);
+    }
+
+    fn iomap(&mut self, index: usize, map: &hw::IOMapping) -> Result {
+        let off = map.base & mmu::UAT_PGMSK;
+        let base = map.base - off;
+        let end = (map.base + map.size + mmu::UAT_PGMSK) & !mmu::UAT_PGMSK;
+        let mapping = self.uat.kernel_context().map_io(base, end - base)?;
+
+        self.initdata.runtime_pointers.hwdata_b.with_mut(|raw, _| {
+            raw.io_mappings[index] = fw::initdata::raw::IOMapping {
+                phys_addr: map.base as u64,
+                virt_addr: (mapping.iova() + off) as u64,
+                size: mapping.size() as u32,
+                range_size: map.range_size as u32,
+                readwrite: map.writable as u64,
+            };
+        });
+
+        self.io_mappings.try_push(mapping)?;
+        Ok(())
+    }
+}
+
+#[versions(AGX)]
+impl GPUManager for GPUManager::ver {
+    fn init(&self) -> Result {
+        let initdata = self.initdata.gpu_va().get();
+        let mut guard = self.rtkit.lock();
+        let rtk = guard.as_mut().unwrap();
+
+        rtk.boot()?;
+        rtk.start_endpoint(EP_FIRMWARE)?;
+        rtk.start_endpoint(EP_DOORBELL)?;
+        rtk.send_message(EP_FIRMWARE, MSG_INIT | (initdata & INIT_DATA_MASK))?;
+
+        Ok(())
+    }
+
+    fn alloc(&mut self) -> &mut KernelAllocators {
+        &mut self.alloc
+    }
+}
+
+#[versions(AGX)]
+unsafe impl Sync for GPUManager::ver {}
+
+#[versions(AGX)]
+unsafe impl Send for GPUManager::ver {}

yunjinlee67/charm-linux

drivers/gpu/drm/asahi/driver.rs
@@ -4,11 +4,10 @@
 //! Driver for the Apple AGX GPUs found in Apple Silicon SoCs.
 
 use kernel::{
-    c_str, device, drm, drm::drv, error::Result, io_mem::IoMem, module_platform_driver, of,
-    platform, prelude::*, soc::apple::rtkit, sync::smutex::Mutex, sync::Arc, PointerWrapper,
+    c_str, device, drm, drm::drv, error::Result, io_mem::IoMem, of, platform, prelude::*, sync::Arc,
 };
 
-use crate::{alloc, fw, gem, initdata, mmu};
+use crate::{gem, gpu, hw, mmu};
 
 use kernel::macros::vtable;
 
@@ -27,9 +26,7 @@ const INFO: drv::DriverInfo = drv::DriverInfo {
 
 pub(crate) struct AsahiData {
     dev: device::Device,
-    uat: crate::mmu::Uat,
-    rtkit: Mutex<Option<rtkit::RTKit<AsahiDevice>>>,
-    initdata: Mutex<fw::types::GpuObject<fw::initdata::InitDataG13GV13_0B4>>,
+    gpu: Arc<dyn gpu::GpuManager>,
 }
 
 pub(crate) struct AsahiResources {
@@ -47,28 +44,6 @@ impl AsahiDevice {
         res.asc.writel_relaxed(val | CPU_RUN, CPU_CONTROL);
 
         Ok(())
-    }
-}
-
-#[vtable]
-impl rtkit::Operations for AsahiDevice {
-    type Data = Arc<DeviceData>;
-    type Buffer = gem::ObjectRef;
-
-    fn shmem_alloc(
-        data: <Self::Data as PointerWrapper>::Borrowed<'_>,
-        size: usize,
-    ) -> Result<Self::Buffer> {
-        let mut guard = data.registrations().ok_or(ENXIO)?;
-        let reg = guard.as_pinned_mut();
-        let dev = reg.device();
-        dev_info!(dev, "shmem_alloc() {:#x} bytes\n", size);
-
-        let mut obj = gem::new_object(dev, size)?;
-        obj.vmap()?;
-        let map = obj.map_into(data.uat.kernel_context())?;
-        dev_info!(dev, "shmem_alloc() -> VA {:#x}\n", map.iova());
-        Ok(obj)
     }
 }
 
@@ -110,39 +85,15 @@ impl platform::Driver for AsahiDevice {
         // Start the coprocessor CPU, so UAT can initialize the handoff
         AsahiDevice::start_cpu(&mut res)?;
 
-        let uat = mmu::Uat::new(&dev)?;
         let reg = drm::drv::Registration::<AsahiDevice>::new(&dev)?;
+        let gpu = gpu::GpuManagerG13GV13_0B4::new(&reg.device(), &hw::t8103::HWCONFIG)?;
 
-        let mut allocator = alloc::SimpleAllocator::new(reg.device(), uat.kernel_context(), 0x20);
-        let mut builder = initdata::InitDataBuilderG13GV13_0B4::new(&mut allocator);
-        let initdata = builder.build(&initdata::HWCONFIG_T8103)?;
-
-        let data = kernel::new_device_data!(
-            reg,
-            res,
-            AsahiData {
-                uat,
-                dev,
-                rtkit: Mutex::new(None),
-                initdata: Mutex::new(initdata),
-            },
-            "Asahi::Registrations"
-        )?;
+        let data =
+            kernel::new_device_data!(reg, res, AsahiData { dev, gpu }, "Asahi::Registrations")?;
 
         let data = Arc::<DeviceData>::from(data);
 
-        {
-            let mut guard = data.registrations().ok_or(ENXIO)?;
-            let reg = guard.as_pinned_mut();
-            let dev = reg.device();
-            dev_info!(dev, "info through dev\n");
-        }
-
-        let mut rtkit =
-            unsafe { rtkit::RTKit::<AsahiDevice>::new(&data.dev, None, 0, data.clone()) }?;
-
-        rtkit.boot()?;
-        *data.rtkit.lock() = Some(rtkit);
+        data.gpu.init()?;
 
         kernel::drm_device_register!(data.registrations().ok_or(ENXIO)?.as_pinned_mut(), (), 0)?;
 

drivers/gpu/drm/asahi/gpu.rs
@@ -1,0 +1,178 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+#![allow(missing_docs)]
+#![allow(dead_code)]
+
+use core::mem;
+
+use kernel::{
+    drm,
+    macros::versions,
+    prelude::*,
+    soc::apple::rtkit,
+    sync::{smutex::Mutex, Arc, UniqueArc},
+    PointerWrapper,
+};
+
+use crate::{alloc, channel, fw, gem, hw, initdata, mmu};
+
+const EP_FIRMWARE: u8 = 0x20;
+const EP_DOORBELL: u8 = 0x21;
+
+const MSG_INIT: u64 = 0x81 << 48;
+const INIT_DATA_MASK: u64 = (1 << 44) - 1;
+
+pub(crate) struct KernelAllocators {
+    pub(crate) private: alloc::SimpleAllocator,
+    pub(crate) shared: alloc::SimpleAllocator,
+    pub(crate) gpu: alloc::SimpleAllocator,
+}
+
+struct RxChannels {
+    stats: channel::StatsChannel,
+}
+
+#[versions(AGX)]
+pub(crate) struct GpuManager {
+    dev: drm::device::Device,
+    initialized: bool,
+    initdata: fw::types::GpuObject<fw::initdata::InitData::ver>,
+    uat: mmu::Uat,
+    alloc: KernelAllocators,
+    io_mappings: Vec<mmu::Mapping>,
+    rtkit: Mutex<Option<rtkit::RTKit<GpuManager::ver>>>,
+    rx_channels: Mutex<RxChannels>,
+}
+
+pub(crate) trait GpuManager: Send + Sync {
+    fn init(&self) -> Result;
+    fn alloc(&mut self) -> &mut KernelAllocators;
+}
+
+#[versions(AGX)]
+#[vtable]
+impl rtkit::Operations for GpuManager::ver {
+    type Data = Arc<GpuManager::ver>;
+    type Buffer = gem::ObjectRef;
+
+    fn recv_message(data: <Self::Data as PointerWrapper>::Borrowed<'_>, ep: u8, msg: u64) {
+        let dev = &data.dev;
+        dev_info!(dev, "RTKit message: {:#x}:{:#x}\n", ep, msg);
+
+        let mut ch = data.rx_channels.lock();
+        ch.stats.poll();
+    }
+
+    fn shmem_alloc(
+        data: <Self::Data as PointerWrapper>::Borrowed<'_>,
+        size: usize,
+    ) -> Result<Self::Buffer> {
+        let dev = &data.dev;
+        dev_info!(dev, "shmem_alloc() {:#x} bytes\n", size);
+
+        let mut obj = gem::new_object(dev, size)?;
+        obj.vmap()?;
+        let map = obj.map_into(data.uat.kernel_context())?;
+        dev_info!(dev, "shmem_alloc() -> VA {:#x}\n", map.iova());
+        Ok(obj)
+    }
+}
+
+#[versions(AGX)]
+impl GpuManager::ver {
+    pub(crate) fn new(
+        dev: &drm::device::Device,
+        cfg: &hw::HwConfig,
+    ) -> Result<Arc<GpuManager::ver>> {
+        let uat = mmu::Uat::new(dev)?;
+        let mut alloc = KernelAllocators {
+            private: alloc::SimpleAllocator::new(dev, uat.kernel_context(), 0x20),
+            shared: alloc::SimpleAllocator::new(dev, uat.kernel_context(), 0x20),
+            gpu: alloc::SimpleAllocator::new(dev, uat.kernel_context(), 0x20),
+        };
+
+        let dyncfg = hw::HwDynConfig {
+            uat_context_table_base: uat.context_table_base(),
+        };
+
+        let mut builder = initdata::InitDataBuilder::ver::new(&mut alloc, cfg, &dyncfg);
+        let initdata = builder.build()?;
+
+        let mut mgr = UniqueArc::try_new(GpuManager::ver {
+            dev: dev.clone(),
+            initialized: false,
+            initdata,
+            uat,
+            io_mappings: Vec::new(),
+            rtkit: Mutex::new(None),
+            rx_channels: Mutex::new(RxChannels {
+                stats: channel::StatsChannel::new(&mut alloc)?,
+            }),
+            alloc,
+        })?;
+
+        let p_stats = mgr.rx_channels.lock().stats.to_raw();
+        mgr.initdata.runtime_pointers.with_mut(|raw, _| {
+            raw.stats = p_stats;
+        });
+
+        for (i, map) in cfg.io_mappings.iter().enumerate() {
+            if let Some(map) = map.as_ref() {
+                mgr.iomap(i, map)?;
+            }
+        }
+
+        let mgr = Arc::from(mgr);
+
+        let rtkit = rtkit::RTKit::<GpuManager::ver>::new(dev, None, 0, mgr.clone())?;
+
+        *mgr.rtkit.lock() = Some(rtkit);
+
+        Ok(mgr)
+    }
+
+    fn iomap(&mut self, index: usize, map: &hw::IOMapping) -> Result {
+        let off = map.base & mmu::UAT_PGMSK;
+        let base = map.base - off;
+        let end = (map.base + map.size + mmu::UAT_PGMSK) & !mmu::UAT_PGMSK;
+        let mapping = self.uat.kernel_context().map_io(base, end - base)?;
+
+        self.initdata.runtime_pointers.hwdata_b.with_mut(|raw, _| {
+            raw.io_mappings[index] = fw::initdata::raw::IOMapping {
+                phys_addr: map.base as u64,
+                virt_addr: (mapping.iova() + off) as u64,
+                size: mapping.size() as u32,
+                range_size: map.range_size as u32,
+                readwrite: map.writable as u64,
+            };
+        });
+
+        self.io_mappings.try_push(mapping)?;
+        Ok(())
+    }
+}
+
+#[versions(AGX)]
+impl GpuManager for GpuManager::ver {
+    fn init(&self) -> Result {
+        let initdata = self.initdata.gpu_va().get();
+        let mut guard = self.rtkit.lock();
+        let rtk = guard.as_mut().unwrap();
+
+        rtk.boot()?;
+        rtk.start_endpoint(EP_FIRMWARE)?;
+        rtk.start_endpoint(EP_DOORBELL)?;
+        rtk.send_message(EP_FIRMWARE, MSG_INIT | (initdata & INIT_DATA_MASK))?;
+
+        Ok(())
+    }
+
+    fn alloc(&mut self) -> &mut KernelAllocators {
+        &mut self.alloc
+    }
+}
+
+#[versions(AGX)]
+unsafe impl Sync for GpuManager::ver {}
+
+#[versions(AGX)]
+unsafe impl Send for GpuManager::ver {}