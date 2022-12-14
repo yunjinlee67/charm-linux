lfnpcsoft/linux-asahi

drm/asahi: driver: Make AsahiData fields public
Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/gpu/drm/asahi/driver.rs
@@ -25,8 +25,8 @@ const INFO: drv::DriverInfo = drv::DriverInfo {
 };
 
 pub(crate) struct AsahiData {
-    dev: device::Device,
-    gpu: Ref<dyn gpu::GPUManager>,
+    pub(crate) dev: device::Device,
+    pub(crate) gpu: Ref<dyn gpu::GPUManager>,
 }
 
 pub(crate) struct AsahiResources {

yunjinlee67/charm-linux

drivers/gpu/drm/asahi/driver.rs
@@ -25,8 +25,8 @@ const INFO: drv::DriverInfo = drv::DriverInfo {
 };
 
 pub(crate) struct AsahiData {
-    dev: device::Device,
-    gpu: Arc<dyn gpu::GpuManager>,
+    pub(crate) dev: device::Device,
+    pub(crate) gpu: Arc<dyn gpu::GpuManager>,
 }
 
 pub(crate) struct AsahiResources {