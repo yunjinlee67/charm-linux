45be4ea7005400ea83c50d1216a7f23b23a43f8f
Hector Martin <marcan@marcan.st>
2023-09-20 22:16:41 +0900

    fixup! rust: drm: gem: Add GEM object abstraction

--- rust/kernel/drm/gem/mod.rs
+++ rust/kernel/drm/gem/mod.rs
@@ -237,6 +237,7 @@ impl<T: DriverObject> Object<T> {
         vmap: None,
         vunmap: None,
         mmap: None,
+        status: None,
         vm_ops: core::ptr::null_mut(),
         evict: None,
     };
